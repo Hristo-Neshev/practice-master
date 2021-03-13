6f71cdf69d83ad52a3861d0708fc344cc9e7f7e3
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2021-03-13 15:39:13 +0200

    Add hover effect and update logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
-  let [loggedIn, setLoggedIn] = useState(true);
+  let [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <div className="App">
--- src/components/componentElements/Logo/Logo.js
+++ src/components/componentElements/Logo/Logo.js
@@ -3,8 +3,8 @@ import './Logo.scss';
 function Logo() {
     return (
         <div className='logo'>
-             <i class="fab fa-product-hunt fa-2x"></i>
-              <h2>Practice Master</h2>
+             <i className="fab fa-product-hunt fa-2x"></i>
+              <a href="#"><h2>Practice Master</h2></a>
         </div>
     )
 }

fe0836bba4765e66c9722066c0cadeba6a1f1c1b
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2021-03-14 11:10:54 +0200

    Change folder structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import './Header.scss';
 import Logo from '../componentElements/Logo/Logo';
-import Navigation from '../Navigation/Navigation';
+import Navigation from '../componentElements/Navigation/Navigation';
 
 function Header(props) {
 

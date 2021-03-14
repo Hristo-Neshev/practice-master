7bbb24c69380f313341143f2fd5fe52f4bdd208c
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2021-03-14 10:54:11 +0200

    Add styles to About Component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,15 @@
 import './About.scss';
 
 function About() {
+    const aboutText = " е органайзер за музиканти. Помага за по-добра организация на работният процес както при професионалисти, така и при начинаещи. Добро помощно средства за учениците от детските музикални школи, както и за тези от музикалните училища."
     return (
         <section className="about">
-            <h1>Какво е Practice Master?</h1>
+            <h1>Какво е <span className='logo-span'>Practice Master</span>?</h1>
             <article className='about-content'>
-                <p>Practice Master е органайзер за музиканти.
-                     Помага за по-добра организация на работният процес както при професионалисти, така и при начинаещи.</p>
+                <p><span className='logo-span'>Practice Master</span>{aboutText} </p>
                 <section className="benefits">
                     <h2>Practice Master помага за:</h2>
-                    <ul>
+                    <ul className='benefits-list'>
                         <li><i class="fas fa-check"></i>Поставяне на конкретни цели</li>
                         <li><i class="fas fa-check"></i>По-лесна организация на ежедневната работа</li>
                         <li><i class="fas fa-check"></i>Водене на бележки</li>

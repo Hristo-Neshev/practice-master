3d7965775c7054ba53b0e4a075bbe2f291690cdb
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2021-03-14 13:41:36 +0200

    Create content and style for Login component

---
+++ src/components/UserComponents/Login/Login.js
@@ -0,0 +1,27 @@
+import './Login.scss';
+
+function Login(props) {
+    return (
+        <article className="login-container">
+            <h1>Влез</h1>
+            <section className="login">
+                <div className="img-container">
+                    <img src="music-notes.jpg" alt="img" />
+                </div>
+                <div className="form-container">
+                    <form action="" className="login-form">
+                        <input type="text" placeholder="Име" />
+                        <input type="password" placeholder="Парола" />
+                        <input className='input-btn' type="submit" value="Влез" />
+                    </form>
+                </div>
+            </section>
+            <div className='create-new-acc'>
+                <h1>Нямаш профил?</h1>
+                <p> Създай нов профил<a href="#">ТУК</a></p>
+            </div>
+        </article>
+    )
+}
+
+export default Login;
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,13 +1,15 @@
 import './Main.scss';
-import About from '../../components/About/About';
+
 import Home from '../../components/Home/Home';
+import Login from '../../components/UserComponents/Login/Login';
 
 function Main(props) {
     const loggedIn = props.loggedIn;
 
     return(
        <main className="main">
-          <Home loggedIn={loggedIn}/>
+          {/* <Home loggedIn={loggedIn}/> */}
+          <Login/>
        </main>
          
     );

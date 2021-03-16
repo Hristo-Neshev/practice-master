eb74e5f13d96b4c707eaad4ad6d6927971991bfd
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2021-03-16 14:37:22 +0200

    Add 404 page and route

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import './NotFound.scss';
+
+function NotFound() {
+    return (
+        <section className='not-found-container'>
+            <img src="404.png" alt="404 not-found" />
+        </section>
+    )
+}
+
+export default NotFound;
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -4,21 +4,23 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../../components/Home/Home';
 import Login from '../../components/UserComponents/Login/Login';
 import Register from '../../components/UserComponents/Register/Register';
+import NotFound from '../../components/NotFound/NotFound';
 
 function Main(props) {
    const loggedIn = props.loggedIn;
 
    return (
       <main className="main">
-      <Switch>
-      <Route path='/' exact><Home loggedIn={loggedIn}/></Route>
-        <Route path='/register' render={()=> (
-           loggedIn ? <Redirect to='/'/> : (<Register/>)
-        )}/>
-          <Route path='/login' render={()=> (
-           loggedIn ? <Redirect to='/'/> : (<Login/>)
-        )}/>
-      </Switch>
+         <Switch>
+            <Route path='/' exact><Home loggedIn={loggedIn} /></Route>
+            <Route path='/register' render={() => (
+               loggedIn ? <Redirect to='/' /> : (<Register />)
+            )} />
+            <Route path='/login' render={() => (
+               loggedIn ? <Redirect to='/' /> : (<Login />)
+            )} />
+            <Route component={NotFound}></Route>
+         </Switch>
       </main>
 
    );
